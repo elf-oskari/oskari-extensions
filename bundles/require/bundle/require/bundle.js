719318df8af318161c0c1650815ae699db90c141
nls-jajuko <janne.korhonen@maanmittauslaitos.fi>
2013-08-19 11:21:54 +0300

    reduced code

--- bundles/require/bundle/require/bundle.js
+++ bundles/require/bundle/require/bundle.js
@@ -1,14 +1,11 @@
 define(["oskari", "./instance", "./Flyout", "./locale/fi", "./locale/en"], function(Oskari, instanceClazz, flyoutClazz) {
 
-	var cid = 'require';
-	
-	var cls = Oskari.cls("Oskari.sample.bundle.require.RequireBundle").
+	return Oskari.cls("Oskari.sample.bundle.require.RequireBundle").
 	  category({
 		"create" : function() {
-			return Oskari.clazz.create(instanceClazz.name(), cid, flyoutClazz.name());
+			return Oskari.clazz.create(instanceClazz.name(), 'require', flyoutClazz.name());
 		}
 	  }).bundle('require'); 
 
-	return cls;
 	
 });
