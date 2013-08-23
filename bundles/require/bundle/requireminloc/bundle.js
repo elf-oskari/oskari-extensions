e121506b4d96e49fc4921b03b1766937faca6da4
nls-jajuko <janne.korhonen@maanmittauslaitos.fi>
2013-08-23 18:58:31 +0300

    under 20 lines-of-code for a bundle in requireminloc poc

---
+++ bundles/require/bundle/requireminloc/bundle.js
@@ -0,0 +1,20 @@
+define(["oskari"], function(Oskari) {
+    return Oskari.bundleCls('requireminloc', "Oskari.sample.bundle.requireminloc.RequireBundle").category({
+        create : function() {
+            return Oskari.cls().extend("Oskari.userinterface.extension.EnhancedExtension").category({
+                startPlugin : function() {
+                    this.setFlyout(Oskari.cls().extend("Oskari.userinterface.extension.EnhancedFlyout").category({
+                        startPlugin : function() {
+                            this.getEl().append("require based 'extreme' implementation with no vars and nested anonymous classes");
+                        }
+                    }).create(this, {
+                        "title" : "require (no-rules-no comments)"
+                    }));
+                    this.setTile(Oskari.cls('Oskari.userinterface.extension.DefaultTile').create(this, {
+                        "title" : "require-loc"
+                    }));
+                }
+            }).create('requireminloc');
+        }
+    })
+});
