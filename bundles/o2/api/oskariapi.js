72f6575623942ca81e2a3aba5e1fcd62896f2f30
nls-jajuko <janne.korhonen@maanmittauslaitos.fi>
2013-10-25 15:39:32 +0300

    ol2 poc finetuned

--- bundles/o2/api/oskariapi.js
+++ bundles/o2/api/oskariapi.js
@@ -4,9 +4,37 @@
  */
 define("oskari", function(Oskari) {
 
-    var cs = Oskari.clazz,
+    var cs = Oskari.clazz;
+    
+    /* Patches required to Oskari 1.0 */
+
+    /* Patch to enable common extend({  methods ... }) type of coding */
+    Oskari.clazz.category('Oskari.ModuleSpec', 'oskari-2.0-API-extend', {
+        extend : function(clsss) {
+
+            var t = typeof clsss;
+
+            if (t === "string") {
+                var clazzInfo = cs.extend(this.clazzName, clsss.length ? clsss : [clsss]);
+                this.clazzInfo = clazzInfo;
+            } else if (t === "object" && clsss._) {
+                this.category(clsss._.categories[clsss._.composition.clazzName]);
+            } else if (t === "object" && clsss.length) {
+                var clazzInfo = cs.extend(this.clazzName, clsss.length ? clsss : [clsss]);
+                this.clazzInfo = clazzInfo;
+            } else {
+                this.category(clsss);
+
+            }
+
+            return this;
+        }
+    });
+    
     /* Simplified Application API for Oskari 2.0 */
-    App = Oskari.cls().methods({
+    var App = Oskari.cls(undefined,function() {
+    	this.instances = {};
+    }).methods({
         setConfiguration : function(c) {
             this.config = c;
             return this;
@@ -39,7 +67,11 @@ define("oskari", function(Oskari) {
 
                 if (me.bundles) {
                     for (var m = 0, mlen = me.bundles.length; m < mlen; m++) {
-                        me.bundles[m].start();
+                        var bundle = me.bundles[m],
+                       		instance = bundle.start(),
+                       		identifier = instance.identifier;
+                        
+                        me.instances[identifier] = instance;
                     }
                 }
 
@@ -55,6 +87,9 @@ define("oskari", function(Oskari) {
             // nop atm
             Oskari.stopApplication();
 
+        },
+        getBundleInstances: function() {
+        	return this.instances;
         }
     });
 
@@ -65,30 +100,20 @@ define("oskari", function(Oskari) {
     Oskari.Tile = Oskari.tileCls();
     Oskari.Flyout = Oskari.flyoutCls();
     Oskari.Extension = Oskari.extensionCls();
-    Oskari.Bundle = Oskari.bundleCls();
-
-    /* Patch to enable common extend({  methods ... }) type of coding */
-    Oskari.clazz.category('Oskari.ModuleSpec', 'oskari-2.0-API-extend', {
-        extend : function(clsss) {
-
-            var t = typeof clsss;
-
-            if (t === "string") {
-                var clazzInfo = cs.extend(this.clazzName, clsss.length ? clsss : [clsss]);
-                this.clazzInfo = clazzInfo;
-            } else if (t === "object" && clsss._) {
-                this.category(clsss._.categories[clsss._.composition.clazzName]);
-            } else if (t === "object" && clsss.length) {
-                var clazzInfo = cs.extend(this.clazzName, clsss.length ? clsss : [clsss]);
-                this.clazzInfo = clazzInfo;
-            } else {
-                this.category(clsss);
-
-            }
-
-            return this;
+    
+    var defaultIdentifier = 0;
+    Oskari.Bundle = Oskari.bundleCls().extend({
+    	create : function() {
+    	   console.log("CREATING BUNDLE INSTANCE ",this.extension,this.identifier,this.locale,this.configuration);
+           var instance = 
+        	   	this.extension.create(this.identifier||'_'+(++defaultIdentifier), this.locale);
+           if( this.configuration ) {
+        	   instance.conf = this.configuration;
+           }
+           return instance;
         }
     });
+
     
     return Oskari;
 
--- bundles/o2/app.js
+++ bundles/o2/app.js
@@ -10,54 +10,53 @@ define([ "./api/oskariapi", "mapfull", "divmanazer" ],
 /* */
 function(Oskari, Mapping, DIVManager) {
 
-	/* Locale, Flyout, Extension and Bundle form a Oskari extension */
+        /* Locale, Flyout, Extension and Bundle form a Oskari extension */
 
-	/* Locale comes from i18n in real life */
-	var locale = {
-		tile : {
-			title : 'X'
-		},
-		flyout : {
-			message : 'Oskari 2.0',
-			title : 'X'
-		}
-	},
+        /* Locale comes from i18n in real life */
+        var locale = {
+           tile : {
+             title : 'X'
+           },
+           flyout : {
+             message : 'Oskari 2.0',
+             title : 'X'
+           }
+        };
 
-	/* Flyout is a UI Component */
-	Flyout = Oskari.Flyout.extend({
-		startPlugin : function() {
-			var el = this.getEl(), msg = this.getLocalization().message;
-			el.append(msg);
-		}
-	}),
+        /* Flyout is a generic UI Component for DivManazer */
+        var Flyout = Oskari.Flyout
+          .extend({
+              startPlugin : function() {
+                   var el = this.getEl(), msg = this.getLocalization().message;
+                   el.append(msg);
+              }
+          });
 
-	/* Extension binds extension to DivManazer */
-	Extension = Oskari.Extension
-			.extend({
-				startPlugin : function() {
-					this.setDefaultTile(this.getLocalization('tile').title);
-					this.setFlyout(Flyout.create(this, this
-							.getLocalization('flyout')));
-				}
-			}),
+        /* Extension is required to bind to DivManazer */
+        var Extension = Oskari.Extension
+            .extend({
+              startPlugin : function() {
+                this.setDefaultTile(this.getLocalization('tile').title);
+                this.setFlyout(Flyout.create(this, this.getLocalization('flyout')));
+              }
+            });
 
-	/* Bundle is a module with a predefined lifecycle: create, start, stop */
-	Bundle = Oskari.Bundle.extend({
-		create : function() {
-			return Extension.create('i18nexxxxtension', locale);
-		}
-	}),
+        /* Bundle is a module with a predefined lifecycle: create, start, stop */
+        var Bundle = Oskari.Bundle
+          .extend({
+        	 extension: Extension,
+        	 locale: locale,
+        	 configuration: { sample: 'setting' }
+          });
 
-	/* AppConfig contains environment specific settings */
-	config = {
+        
+        // App
+        Oskari.Application
+          .setBundles([ Mapping, DIVManager, Bundle ])
+          .start()
+          .success(function() {
+            console.log("running");
 
-	};
-
-	// App
-	Oskari.Application.setBundles([ Mapping, DIVManager, Bundle ])
-			.setConfiguration(config).start().success(function() {
-				console.log("running");
-
-			});
+          });
 
 });
